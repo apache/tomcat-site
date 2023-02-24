7f02238442fb53f1a9c1bd2be2684031ab9a048f
Christopher Schultz <schultz@apache.org>
2023-02-24 17:51:51 +0000

    Update website to include v10.1.6.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/site/trunk@1907858 13f79535-47bb-0310-9956-ffa450edef68

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-vi build.properties.default xdocs/download-80.xml xdocs/index.xml xdocs/oldnews.xml xdocs/whichversion.xml xdocs/migration-85.xml xdocs/doap_Tomcat.rdf
+RELEASE=${1:-8.5}
+MAJOR_RELEASE=$( expr "${RELEASE}" : '^\([0-9]*\)' )
+if [ "8" = "${MAJOR_RELEASE}" ] ; then
+  DOWNLOAD_FILENAME=xdocs/download-80.xml
+elif [ "9" = "${MAJOR_RELEASE}" ] ; then
+  DOWNLOAD_FILENAME=xdocs/download-90.xml
+else
+  DOWNLOAD_FILENAME=xdocs/download-${MAJOR_RELEASE}.xml
+fi
+
+if [ "8.5" = "${RELEASE}" ] ; then 
+  MIGRATION_FILENAME=xdocs/migration-85.xml
+elif [ "9.0" = "${RELEASE}" ] ; then
+  MIGRATION_FILENAME=xdocs/migration-90.xml
+else
+  MIGRATION_FILENAME=xdocs/migration-${RELEASE}.xml
+fi
+
+vi build.properties.default "${DOWNLOAD_FILENAME}" xdocs/index.xml xdocs/oldnews.xml xdocs/whichversion.xml "${MIGRATION_FILENAME}" xdocs/doap_Tomcat.rdf
