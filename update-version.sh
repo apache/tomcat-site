c85bd09eef175c20774e248ab93b6346b4aea275
Christopher Schultz <schultz@apache.org>
2023-02-24 17:25:01 +0000

    Update site to include v8.5.86 release.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/site/trunk@1907856 13f79535-47bb-0310-9956-ffa450edef68

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-vi xdocs/download-80.xml xdocs/index.xml xdocs/oldnews.xml xdocs/whichversion.xml xdocs/migration-85.xml xdocs/doap_Tomcat.rdf
+vi build.properties.default xdocs/download-80.xml xdocs/index.xml xdocs/oldnews.xml xdocs/whichversion.xml xdocs/migration-85.xml xdocs/doap_Tomcat.rdf
