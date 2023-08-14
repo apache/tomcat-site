f92ba2e4a55175f85c113f6ddab2c70b796924c7
Mark Thomas <markt@apache.org>
2023-08-14 13:20:45 +0000

    Update docs for 10.1.12 release
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/site/trunk@1911646 13f79535-47bb-0310-9956-ffa450edef68

--- docs/tomcat-10.1-doc/websocketapi/package-search-index.js
+++ docs/tomcat-10.1-doc/websocketapi/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jakarta.websocket"},{"l":"jakarta.websocket.server"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jakarta.websocket"},{"l":"jakarta.websocket.server"}];updateSearchResults();
