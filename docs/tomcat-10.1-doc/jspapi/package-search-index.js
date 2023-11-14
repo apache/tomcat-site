1f8633e0bf058c208e41a68e78ff2d47066d54a7
Christopher Schultz <schultz@apache.org>
2023-11-14 22:56:00 +0000

    Add Tomcat 10.1.16 to news page.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/site/trunk@1913786 13f79535-47bb-0310-9956-ffa450edef68

--- docs/tomcat-10.1-doc/jspapi/package-search-index.js
+++ docs/tomcat-10.1-doc/jspapi/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jakarta.servlet.jsp"},{"l":"jakarta.servlet.jsp.el"},{"l":"jakarta.servlet.jsp.tagext"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jakarta.servlet.jsp"},{"l":"jakarta.servlet.jsp.el"},{"l":"jakarta.servlet.jsp.tagext"}];updateSearchResults();
